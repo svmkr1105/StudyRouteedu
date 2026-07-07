2a06ca3fddbf02cc0bfad99f36d35daa3e28bbd6
svmkr1105 <sk483680@gmail.com>
2026-07-07 11:33:39 +0530

    Add main.js in other of the local host

--- js/main.js
+++ js/main.js
@@ -11,5 +11,5 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // API Base URL
 
-const API_BASE = 'http://localhost:8000/api';
-// const API_BASE = 'https://studyroute-backend.onrender.com/api';
+// const API_BASE = 'http://localhost:8000/api';
+const API_BASE = 'https://studyroute-backend.onrender.com/api';
