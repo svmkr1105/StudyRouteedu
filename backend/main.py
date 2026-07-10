29103948bc60e20ecee308e5d8202d883a2a22e5
svmkr1105 <sk483680@gmail.com>
2026-07-10 09:15:57 +0530

    main.py update

--- backend/main.py
+++ backend/main.py
@@ -27,7 +27,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://studyrouteeducation.netlify.app",
+        "https://studyrouteedu.netlify.app",
         "https://studyrouteedu.onrender.com",
         "https://studyroute-portal.onrender.com",
         "http://localhost:5500",
