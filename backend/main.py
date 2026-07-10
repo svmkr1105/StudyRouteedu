60f81f76d3804dde07904ba39becb88562e9efa8
svmkr1105 <sk483680@gmail.com>
2026-07-10 08:40:45 +0530

    comment localhost

--- backend/main.py
+++ backend/main.py
@@ -30,8 +30,8 @@ app.add_middleware(
         "https://studyrouteeducation.netlify.app",
         "https://studyroute-education-portal.onrender.com",
         "https://studyroute-portal.onrender.com",
-        "http://localhost:5500",
-        "http://127.0.0.1:5500"
+        # "http://localhost:5500",
+        # "http://127.0.0.1:5500"
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
