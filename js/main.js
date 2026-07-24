6913fec509f56c0b3e5a3e84056aa84b572b1f88
svmkr1105 <sk483680@gmail.com>
2026-07-24 18:49:18 +0530

    New add main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
-// Navbar Toggle
+// // Navbar Toggle
+// document.addEventListener('DOMContentLoaded', function() {
+//     const toggle = document.getElementById('navToggle');
+//     const links = document.getElementById('navLinks');
+//     if (toggle && links) {
+//         toggle.addEventListener('click', () => {
+//             links.classList.toggle('active');
+//         });
+//     }
+// });
+
+// // API Base URL
+
+// // const API_BASE = 'http://localhost:8000/api';
+// const API_BASE = 'https://studyrouteedu.onrender.com/api'
+
+
+
+
+// ===== API BASE URL =====
+const API_BASE = 'https://studyrouteedu.onrender.com/api';
+
+// ===== NAVBAR TOGGLE =====
 document.addEventListener('DOMContentLoaded', function() {
     const toggle = document.getElementById('navToggle');
     const links = document.getElementById('navLinks');
@@ -8,8 +30,3 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
-
-// API Base URL
-
-// const API_BASE = 'http://localhost:8000/api';
-const API_BASE = 'https://studyrouteedu.onrender.com/api'
